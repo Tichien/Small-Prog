8b0391f36e32eb076d8fc84700f67be92f38f8bd
Thibault Odorico <odoricothibault@gmail.com>
2018-03-20 12:36:19 +0100

    Mise à jour du mar. 20 mars 2018 12:36:19

--- Curses/main.c
+++ Curses/main.c
@@ -7,33 +7,67 @@
 #include <form.h>
 #include <locale.h>
 
+int braille_char_offset = 0x2800;
+
+int pixel_map[4][2] = { {0x01, 0x08}, 
+                        {0x02, 0x10},
+                        {0x04, 0x20},
+                        {0x40, 0x80} };
 
 int main()
 {
+
+        int k = 0;
+        scanf("%d", &k);
+
+        int data[k][2];
+
+        for(int i = 0 ; i < k ; i++){
+                data[i][0] = -1;
+                data[i][1] = -1;
+        }
+
+        for(int i = 0 ; i < k ; i++){
+                scanf("%d%d", &data[i][0], &data[i][1]);
+        }
+
+        int braille = 0x2800;
+        //printw("%u", i);
         /* Initialize curses */
-		setlocale(LC_ALL, "");
+        setlocale(LC_ALL, "");
+
         initscr();
         start_color();
-        cbreak();
+        //cbreak();
         noecho();
         keypad(stdscr, TRUE);
 
         /* Initialize few color pairs */
-        init_pair(1, COLOR_RED, COLOR_BLACK);
+        init_pair(1, COLOR_GREEN, COLOR_BLACK);
 
         attron(COLOR_PAIR(1));
-		addstr("\u28FF");
-		addstr("\u28FF");
-		addstr("\u28FF");
-		addstr("\u28FF");
-		addstr("\u28FF");
-		addstr("\n");
-		addstr("\u28FF");
-		addstr("\u28FF");
-		addstr("\u28FF");
+
+        for (int i = 0; i < k; ++i){
+                int x = data[i][0], y = data[i][1];
+
+                if(x != -1 && y != -1){
+                        braille += pixel_map[y][x];
+                }
+
+
+        }
+
+        printw("%lc", braille);
+
         attroff(COLOR_PAIR(1));
+       
         refresh();
-        getch();
+
+        int c = 0;
+
+        while((c = getch()) != KEY_F(1)){
+                //printw("%d", c);
+        }
 
         endwin();
         return 0;
