4ef3c878e475ed2f3a86a5d214d4326dfe54dd62
Odorico Thibault <thibault.odorico@etu.umontpellier.fr>
2018-03-28 11:18:54 +0200

    Creation des fonction von_koch, poly_von_koch

--- Curses/main.cpp
+++ Curses/main.cpp
@@ -7,12 +7,62 @@ using namespace std;
 
 void handler(int ch, Vector2i& position, IntRect& offset);
 
+
+
+
+void draw_von_koch_rec(Turtle& T, float length, int prof){
+	if(prof == 0){
+		T.draw(length);
+	}	
+	else{
+		draw_von_koch_rec(T, length/3, prof-1); T.turn(60);	
+	    draw_von_koch_rec(T, length/3, prof-1); T.turn(-120);
+	    draw_von_koch_rec(T, length/3, prof-1); T.turn(60);
+	    draw_von_koch_rec(T, length/3, prof-1); 
+	}
+}
+
+void draw_von_koch(Canvas& c, const Vector2i& pos, float length, int prof){
+	Turtle T(&c);
+
+	T.set_position(pos.x, pos.y);
+
+	draw_von_koch_rec(T, length, prof);
+}
+
+
+void draw_poly_von_koch(Canvas& canvas, Vector2f center, int sides, float radius, float rotation, float prof){
+	
+	Turtle T(&canvas);
+
+	float angle = float(360) / sides;
+	float length = 2 * radius * sin(M_PI / float(sides));
+	float inradius = radius * cos(M_PI / float(sides));
+	Vector2f offset(length / 2.0, inradius);
+
+	//canvas->set(round(center.x), round(center.y));
+
+	T.set_position(center);
+	T.turn(rotation);
+
+	T.move(-length / 2.0);
+	T.turn(90);
+	T.move(-inradius);
+	T.turn(-90);
+
+	for(int i = 0 ; i < sides ; i++){
+		draw_von_koch_rec(T, length, prof);
+		T.turn(angle);
+	}
+}
+
 int main(int argc, char** argv)
 {
 
 	int sides = 3;
 	float radius = 50;
 	float rotation = 0;
+	int prof = 0;
 
 	if(argc > 1)
 		sides = atoi(argv[1]);
@@ -23,12 +73,17 @@ int main(int argc, char** argv)
 	if(argc > 3)
 		rotation = atof(argv[3]);
 
+	if(argc > 4)
+		prof = atoi(argv[4]);
+
 	Vector2f center(radius, radius);
     
 	Term::init_curs();
 
     int k = 400;
-	Canvas canvas;//(radius * 2 + 1, radius * 2 + 1);
+	Canvas canvas;
+
+	//(radius * 2 + 1, radius * 2 + 1);
 	/*
 	for(int i = 0 ; i < canvas.get_size().x; i++){
 		for(int j = 0 ; j < canvas.get_size().y ; j++){
@@ -36,16 +91,20 @@ int main(int argc, char** argv)
 		}
 	}
 	*/
+
 	mvwprintw(stdscr, 0, 0, "Canvas.size = (%d, %d)", canvas.get_size().x, canvas.get_size().y);
 	mvwprintw(stdscr, 1, 0, "Canvas.dimension = (%d, %d)", canvas.get_dimension().x, canvas.get_dimension().y);
 
-	canvas.set_color(ColorPair::Red);
-	canvas.set_attr(Attr::HalfBright);
+	canvas.set_color(ColorPair::Green);
+	canvas.set_attr(Attr::Bright);
 
 	Vector2i p1(10, 20);
 	Vector2i p2(135, 97);
+
+
+	//draw_von_koch(canvas, Vector2i(0, 100), 530, 4);
 	
-	draw_polygon(canvas, center, sides, radius, rotation);
+	draw_poly_von_koch(canvas, center, sides, radius, rotation, prof);
 
 	//Turtle T(&canvas);
 
